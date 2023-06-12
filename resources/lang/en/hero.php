92b272084f6e58691bf6104c28607c022c0c72c9
anarch02 <mrjianarchist@gmail.com>
2023-06-13 00:27:11 +0500

    hero title

--- resources/lang/en/hero.php
+++ resources/lang/en/hero.php
@@ -1,5 +1,5 @@
 <?php 
     return [
-        'title' => 'Unleashing the Power of Digital Innovation',
-        'prev' => 'Welcome to the world of reliable web applications! I am a solo developer specializing in creating high-quality and innovative web applications. I am ready to offer you a unique solution that fully meets your needs and expectations. By developing web applications using advanced technologies and best practices, I guarantee reliability, security, and high performance. Allow me to turn your ideas into reality and provide you with a reliable web application that will help you succeed in your business.'
+        'title' => 'Welcome to the world of reliable web applications',
+        'prev' => 'Unleashing the Power of Digital Innovation! I am a solo developer specializing in creating high-quality and innovative web applications.'
     ];
